3f01197809ef16a2973b5156b3c801d097bfced1
araw604 <vikrr1986@gmail.com>
2016-09-23 15:26:48 -0500

    trying to fix routing

--- scripts/main.js
+++ scripts/main.js
@@ -297,7 +297,7 @@ var StorePicker = React.createClass({
     event.preventDefault();
     // get the data from the input
     var storeId = this.refs.storeId.value;
-    this.history.pushState(null, '/store/' + storeId);
+    this.history.pushState(null, 'react_proj/store/' + storeId);
   },
   render : function() {
     return (
